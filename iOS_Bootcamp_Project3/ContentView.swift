f8e488bc87989bf35a4666d2221b210b25002e9c
Siyu Liu <siyuliu@unc.edu>
2026-02-25 13:23:28 -0500

    Initial Commit

---
+++ iOS_Bootcamp_Project3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Bootcamp_Project3
+//
+//  Created by Siyu Liu on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_Bootcamp_Project3/iOS_Bootcamp_Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Bootcamp_Project3App.swift
+//  iOS_Bootcamp_Project3
+//
+//  Created by Siyu Liu on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Bootcamp_Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
